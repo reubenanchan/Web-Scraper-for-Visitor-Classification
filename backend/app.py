c5dedd45cb2bd82763ce1f9b439c35e3a15ee3f2
reubenanchan <71098011+reubenanchan@users.noreply.github.com>
2024-11-06 14:42:32 -0500

    Initial commit with Flask setup

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-app.r
-
-@app.route('/members')
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+@app.route('/')
+def home():
+    return "Hello, world!"
 
 if __name__ == '__main__':
     app.run(debug=True)
